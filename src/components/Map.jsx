89e2b90a556bf89c1d9b29edb968732839d45704
swapnildubey29 <swapnildubey3636@gmail.com>
2025-01-29 17:38:07 +0530

    Update

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,41 +1,37 @@
-import { useState } from 'react'
-import GoogleMapReact from 'google-map-react'
-import LocationMarker from './LocationMarker'
-import LocationInfoBox from './LocationInfoBox'
+import { useState } from 'react';
+import GoogleMapReact from 'google-map-react';
+import LocationMarker from './LocationMarker';
+import LocationInfoBox from './LocationInfoBox';
 
-// define constants
+// Define constants
 const NATURAL_EVENT_WILDFIRE = 8;
 
-const Map = ({ eventData, center, zoom }) => {
-    const [locationInfo, setLocationInfo] = useState(null)
+const Map = ({ eventData, center = { lat: 42.3265, lng: -122.8756 }, zoom = 4 }) => {
+    const [locationInfo, setLocationInfo] = useState(null);
 
-    const markers = eventData.map((ev, index) => {
-        if(ev.categories[0].id === NATURAL_EVENT_WILDFIRE) {
-            return <LocationMarker key={index} lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
-        }
-        return null
-    })
+    const markers = eventData.map((ev, index) => (
+        ev.categories[0].id === NATURAL_EVENT_WILDFIRE && (
+            <LocationMarker
+                key={index}
+                lat={ev.geometries[0].coordinates[1]}
+                lng={ev.geometries[0].coordinates[0]}
+                onClick={() => setLocationInfo({ id: ev.id, title: ev.title })}
+            />
+        )
+    ));
 
     return (
         <div className="map">
             <GoogleMapReact
-                bootstrapURLKeys={{ key: 'AIzaSyBNJx717_65WCzOSBBObL8-oLGh9IMwYcI' }}
-                defaultCenter={ center }
-                defaultZoom={ zoom }
+                bootstrapURLKeys={{ key: 'Google_API_KEY' }}
+                defaultCenter={center}
+                defaultZoom={zoom}
             >
                 {markers}
             </GoogleMapReact>
             {locationInfo && <LocationInfoBox info={locationInfo} />}
         </div>
-    )
-}
+    );
+};
 
-Map.defaultProps = {
-    center: {
-        lat: 42.3265,
-        lng: -122.8756
-    },
-    zoom: 6
-}
-
-export default Map
+export default Map;
